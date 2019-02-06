5a974883fad387dc57df5fec499d11f7c6a8e4d1
Tadas Kalvaitis <tadas.k31@gmail.com>
2019-02-06 11:20:31 +0200

    Created menu and world selection

---
+++ Assets/Scripts/menu/menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+     public void TaskOnPlayClick()
+    {
+        SceneManager.LoadScene("WorldSelection");
+    }
+
+    public void TaskOnExitClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/menu/worldSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class worldSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TaskOnWorldOneClick()
+    {
+        SceneManager.LoadScene("World1");
+    }
+    public void TaskOnWorldTwoClick()
+    {
+        SceneManager.LoadScene("World2");
+    }
+    public void TaskOnWorldThreeClick()
+    {
+        SceneManager.LoadScene("World3");
+    }
+}
