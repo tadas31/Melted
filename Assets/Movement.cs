f388cd6a72034e9cef0f196fbeb424679a40a703
karolissto <ltkarolissto@gmail.com>
2019-02-18 11:00:32 +0200

    some fixes

---
+++ Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
