45d5186caf1d7cc731cb7beb66e077dcc23898b9
Tadas Kalvaitis <t.kalvaitis@ktu.edu>
2019-04-06 11:24:38 +0300

    More particles (fire, enemy bullet)

---
+++ Assets/Scripts/Weapons/EnemyBulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Weapons/EnemyGun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+    public bool isFiring = true;
+
+    public EnemyBulletController bullet;
+    public float bulletSpeed;
+
+    public float timeBetweenShots;
+    private float shotsTimer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFiring)
+        {
+            shotsTimer -= Time.deltaTime;
+            if (shotsTimer <= 0)
+            {
+                shotsTimer -= Time.deltaTime;
+                if (shotsTimer <= 0)
+                {
+                    shotsTimer = timeBetweenShots;
+                    EnemyBulletController newBullet = Instantiate(bullet, transform.position, transform.rotation) as EnemyBulletController;
+                    newBullet.speed = bulletSpeed;
+                }
+            }
+        }
+        else
+        {
+            if (shotsTimer > 0)
+                shotsTimer -= Time.deltaTime;
+        }
+    }
+}
